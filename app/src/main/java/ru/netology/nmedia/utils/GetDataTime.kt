edad2735db8eda93aa13074d72e0f38f800a9965
Information Technology <it@eugc.ru>
2023-01-30 10:20:21 +0300

    include getDateTime

--- app/src/main/java/ru/netology/nmedia/utils/GetDataTime.kt
+++ app/src/main/java/ru/netology/nmedia/utils/GetDataTime.kt
@@ -1,11 +1,8 @@
 package ru.netology.nmedia.utils
 
-import java.time.ZoneId
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
+import java.text.SimpleDateFormat
+import java.util.*
 
-fun getDataTime() {
-    val timestamp = ZonedDateTime.now(ZoneId.of("Europa/Moscow"))
-        .format(DateTimeFormatter.ofPattern("MM.dd.yyy hh.mm.ss a"))
-
+class GetDataTime {
+    val dateFormat: String? = SimpleDateFormat("dd MMM yyyy, HH:mm:ss z", Locale.ENGLISH).format(Date())
 }
