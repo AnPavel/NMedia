9043d219f01bb237afd5d42621e9b40c2646a6d8
Information Technology <it@eugc.ru>
2023-02-20 17:27:39 +0300

    üpdate 1902_1

--- app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -7,9 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.internal.ViewUtils.hideKeyboard
-import ru.netology.nmedia.databinding.FragmentFeedBinding
-import ru.netology.nmedia.utils.AndroidUtils
+import ru.netology.nmedia.databinding.FragmentNewPostBinding
 import ru.netology.nmedia.utils.StringArg
 import ru.netology.nmedia.viewmodel.PostViewModel
 
@@ -24,16 +22,17 @@ class NewPostFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentFeedBinding.inflate(layoutInflater, container, false)
+        val binding = FragmentNewPostBinding.inflate(layoutInflater, container, false)
         //arguments?.textArg?.let {
         //    binding.edit.settext(it)
         //}
 
         arguments?.textArg?.let(binding.content::setText)
         binding.content.requestFocus()
-        binding.add.setOnClickListener {
+
+        binding.buttonOk.setOnClickListener {
             val content = binding.content.text.toString()
-            if (content.isNullorBlank()) {
+            if (content.isNullOrEmpty()) {
                 viewModel.changeContent(content)
                 viewModel.save()
                 //AndroidUtils.hideKeyboard(requireView())
