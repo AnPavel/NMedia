8b4a900e71bdecb71dde94e1f4656f5964bd015c
Information Technology <it@eugc.ru>
2023-02-24 15:31:30 +0300

    del PostDaoImpl and PostRepositorySQLiteImpl

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -22,7 +22,10 @@ private val empty = Post(
 
 class PostViewModel(application: Application): AndroidViewModel(application) {
 
-    private val repository: PostRepository = PostRepositorySQLiteImpl(AppDb.getInstance(application).postDao)
+    private val repository: PostRepository = PostRepositoryImpl(
+        AppDb.getInstance(context = application).postDao()
+    )
+    //private val repository: PostRepository = PostRepositorySQLiteImpl(AppDb.getInstance(application).postDao)
     //private val repository: PostRepository = PostRepositoryInMemoryImplementation()
     //private val repository: PostRepository = PostRepositoryFileImpl(application)
     //private val repository: PostRepository = PostRepositorySharedPrefsImpl(application)
