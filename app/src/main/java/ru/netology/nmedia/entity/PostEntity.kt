f66c747f20683872c584dbb20f6e4296be13ef36
Information Technology <it@eugc.ru>
2023-02-24 13:46:49 +0300

    edit PostEntity

--- app/src/main/java/ru/netology/nmedia/entity/PostEntity.kt
+++ app/src/main/java/ru/netology/nmedia/entity/PostEntity.kt
@@ -9,16 +9,38 @@ data class PostEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
     val author: String,
-    val content: String,
     val published: String,
+    val content: String,
     val likedByMe: Boolean,
-    val likes: Int = 0,
+    val countFavorite: Int = 0,
+    val countShare: Int = 0,
+    val countRedEye: Int = 0,
+    val linkToVideo: String = "",
 ) {
-    fun toDto() = Post(id, author, content, published, likedByMe, likes)
+    fun toDto() = Post(
+        id,
+        author,
+        published,
+        content,
+        likedByMe,
+        countFavorite,
+        countShare,
+        countRedEye,
+        linkToVideo
+    )
 
     companion object {
         fun fromDto(dto: Post) =
-            PostEntity(dto.id, dto.author, dto.content, dto.published, dto.likedByMe, dto.likes)
-
+            PostEntity(
+                dto.id,
+                dto.author,
+                dto.publisher,
+                dto.content,
+                dto.likedByMe,
+                dto.countFavorite,
+                dto.countShare,
+                dto.countRedEye,
+                dto.linkToVideo
+            )
     }
 }
