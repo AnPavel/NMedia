0951982f8d518296709c876d9f0a5d72c6ed10cc
Information Technology <it@eugc.ru>
2023-08-13 23:20:01 +0300

    update AppActivity add FirebaseInstallation and FirebaseMessaging

--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -16,6 +16,7 @@ import androidx.core.view.MenuProvider
 import androidx.navigation.findNavController
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
+import com.google.firebase.installations.FirebaseInstallations
 import com.google.firebase.messaging.FirebaseMessaging
 import ru.netology.nmedia.R
 import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
@@ -25,7 +26,6 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         intent?.let {
             if (it.action != Intent.ACTION_SEND) {
@@ -47,12 +47,38 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
                 )
         }
 
+
+        // 13.08.2023 Firebase -1
+        FirebaseInstallations.getInstance().id.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                println(" - some stuff happened: ${task.exception}")
+                return@addOnCompleteListener
+            }
+
+            val token = task.result
+            Log.d("MyAppLog", "AppActivity * FirebaseInstallations: $token")
+            println(token)
+        }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                println(" - some stuff happened: ${task.exception}")
+                return@addOnCompleteListener
+            }
+
+            val token = task.result
+            Log.d("MyAppLog", "AppActivity * FirebaseMessaging: $token")
+            println(token)
+        }
+        // 13.08.2023 Firebase -2
+
+        //проверка на доступность Google сервисов
         checkGoogleApiAvailability()
         //разрешение на выполнение уведомлений
         requestNotificationsPermission()
 
         val nomVersionApp = System.getProperty("os.version")
-        Log.d("MyAppLog","AppActivity * OS: $nomVersionApp")
+        Log.d("MyAppLog", "AppActivity * OS: $nomVersionApp")
 
         val authViewModel: AuthViewModel by viewModels()
 
@@ -66,6 +92,13 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
             addMenuProvider(object : MenuProvider {
                 override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
                     menuInflater.inflate(R.menu.menu_auth, menu)
+
+                    menu.let {
+                        it.setGroupVisible(R.id.unauthorized, !authViewModel.isAuthorized)
+                        it.setGroupVisible(R.id.authorized, authViewModel.isAuthorized)
+                    }
+
+                    /* старый вариант больше кода
                     val authorized = authViewModel.isAuthorized
                     if(authorized){
                         menu.setGroupVisible(R.id.authorized, true)
@@ -74,24 +107,31 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
                         menu.setGroupVisible(R.id.authorized, false)
                         menu.setGroupVisible(R.id.unauthorized, true)
                     }
+                    */
                 }
 
                 override fun onMenuItemSelected(item: MenuItem): Boolean =
                     when (item.itemId) {
                         R.id.sign_in -> {
-                            Log.d("MyAppLog","AppActivity * menu sign_in - Авторизация пользователя через логин и пароль")
+                            Log.d(
+                                "MyAppLog",
+                                "AppActivity * menu sign_in - Авторизация пользователя через логин и пароль"
+                            )
                             findNavController(R.id.activity_app_layout).navigate(R.id.signInFragment)
                             true
                         }
 
                         R.id.sign_up -> {
-                            Log.d("MyAppLog","AppActivity * menu sign_up - Ввод данных нового пользователя")
+                            Log.d(
+                                "MyAppLog",
+                                "AppActivity * menu sign_up - Ввод данных нового пользователя"
+                            )
                             findNavController(R.id.activity_app_layout).navigate(R.id.userRegNewFragment)
                             true
                         }
 
                         R.id.logout -> {
-                            Log.d("MyAppLog","AppActivity * menu logout")
+                            Log.d("MyAppLog", "AppActivity * menu logout")
                             findNavController(R.id.activity_app_layout).navigate(R.id.logoutDialog)
                             true
                         }
@@ -102,6 +142,7 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
             }.apply {
                 oldMenuProvider = this
             }, this)
+
         }
 
     }
@@ -122,9 +163,12 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
                 .show()
         }
 
+        /* 13.08.2023 Firebase -> FirebaseMessaging
         FirebaseMessaging.getInstance().token.addOnSuccessListener {
+            println("------------------")
             println(it)
         }
+        */
     }
 
     private fun requestNotificationsPermission() {
